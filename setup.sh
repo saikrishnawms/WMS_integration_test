90678b635e7193b15ee70d261a9850d25618a3ea
Nishi Shriuvastava <nishishrivastava.91@gmail.com>
2020-05-15 03:14:09 +0200

    sD

--- setup.sh
+++ setup.sh
@@ -3,8 +3,9 @@ sudo apt-get install -y unzip xvfb libxi6 libgconf-2-4
 sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
 sudo echo "deb [arch=amd64]  http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
 sudo apt-get -y update
-sudo apt-get -y install google-chrome-stable
-sudo apt-get install chromium-browser
+sudo apt-get install libxss1 libappindicator1 libindicator7 fonts-liberation
+sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome*.deb
 sudo wget https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip
 sudo unzip chromedriver_linux64.zip
 sudo mv chromedriver /usr/bin/chromedriver
