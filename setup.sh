56589c5429cac7c165a443657ee7f1e6b71d6e38
Nishi Shriuvastava <nishishrivastava.91@gmail.com>
2020-05-15 03:16:44 +0200

    asd

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ sudo apt-get install -y unzip xvfb libxi6 libgconf-2-4
 sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
 sudo echo "deb [arch=amd64]  http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
 sudo apt-get -y update
-sudo apt-get install libxss1 libappindicator1 libindicator7 fonts-liberation
+sudo apt-get install libxss1 libappindicator1 libindicator7 fonts-liberation libglib2.0-0=2.50.3-2 libnss3=2:3.26.2-1.1+deb9u1 libgconf-2-4=3.2.6-4+b1 libfontconfig1=2.11.0-6.7+b1
 sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome*.deb
 sudo wget https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip
