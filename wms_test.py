0a80edd6d3944830afae35ee9852aded41e59d57
Nishi Shriuvastava <nishishrivastava.91@gmail.com>
2020-05-17 02:42:53 +0200

    hellow

--- wms_test.py
+++ wms_test.py
@@ -6,30 +6,12 @@ from selenium.webdriver.chrome.options import Options
 
 
 class MyTestCase(unittest.TestCase):
-    def test_create_new_user_with_G41(self):
-        driver = webdriver.Chrome(ChromeDriverManager().install())
-        driver.get("http://test.saikrishnacoldstorage.in/lot")
-        driver.find_element_by_name("name").send_keys("Nishi")
-        driver.find_element_by_name("fatherName").send_keys("Shrivastava")
-        driver.find_element_by_name("address").send_keys("Bartiningelle")
-        driver.find_element_by_name("phoneNumber").send_keys("8380083195")
-        driver.find_element_by_name("numberOfBags").send_keys(100)
-        driver.find_element_by_name("averageWeight").send_keys(50)
-        driver.find_element_by_name("numberOfEmptyBagsGiven").send_keys(9)
-        driver.find_element_by_xpath("/html/body/div/div/div/form/div/div[9]/fieldset/div/label[1]/span[1]/span[1]/input").click()
-        driver.find_element_by_name("palledariPaid").click()
-        driver.find_element_by_name("comments").send_keys("test")
-        driver.find_element_by_class_name("MuiButton-label").click()
-        driver.close()
-        driver.quit()
-
-
-
-
-
 
     def test_create_new_user_with_G4(self):
-        driver = webdriver.Chrome(ChromeDriverManager().install())
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        path = "/usr/bin/chromedriver"
+        driver = webdriver.Chrome(executable_path=path, options=chrome_options)
         driver.get("http://test.saikrishnacoldstorage.in/lot")
         driver.find_element_by_name("name").send_keys("Nishi")
         driver.find_element_by_name("fatherName").send_keys("Shrivastava")
@@ -47,7 +29,10 @@ class MyTestCase(unittest.TestCase):
         driver.quit()
 
     def test_create_new_user_with_Gola(test_navigate_into_Url):
-        driver = webdriver.Chrome(ChromeDriverManager().install())
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        path = "/usr/bin/chromedriver"
+        driver = webdriver.Chrome(executable_path=path, options=chrome_options)
         driver.get("http://test.saikrishnacoldstorage.in/lot")
         driver.find_element_by_name("name").send_keys("Nishi")
         driver.find_element_by_name("fatherName").send_keys("Shrivastava")
@@ -66,7 +51,10 @@ class MyTestCase(unittest.TestCase):
         driver.quit()
 
     def test_create_new_user_with_other(self):
-        driver = webdriver.Chrome(ChromeDriverManager().install())
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        path = "/usr/bin/chromedriver"
+        driver = webdriver.Chrome(executable_path=path, options=chrome_options)
         driver.get("http://test.saikrishnacoldstorage.in/lot")
         driver.find_element_by_name("name").send_keys("Nishi")
         driver.find_element_by_name("fatherName").send_keys("Shrivastava")
@@ -84,19 +72,28 @@ class MyTestCase(unittest.TestCase):
         driver.quit()
 
     def test_title(self):
-        driver = webdriver.Chrome(ChromeDriverManager().install())
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        path = "/usr/bin/chromedriver"
+        driver = webdriver.Chrome(executable_path=path, options=chrome_options)
         driver.get("http://test.saikrishnacoldstorage.in/lot")
         assert 'React App' == driver.title
         driver.close()
 
     def test_with_print(self):
-        driver = webdriver.Chrome(ChromeDriverManager().install())
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        path = "/usr/bin/chromedriver"
+        driver = webdriver.Chrome(executable_path=path, options=chrome_options)
         driver.get("http://test.saikrishnacoldstorage.in/showLot/5")
         driver.find_element_by_xpath("/html/body/div/div/div/div/div[13]/button").click()
         driver.close()
 
     def test_with_result(self):
-        driver = webdriver.Chrome(ChromeDriverManager().install())
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        path = "/usr/bin/chromedriver"
+        driver = webdriver.Chrome(executable_path=path, options=chrome_options)
         driver.get("http://test.saikrishnacoldstorage.in/showLot/5")
         averageWeight = driver.find_element_by_xpath("/html/body/div/div/div/div/div[8]")
         print(averageWeight.text)
