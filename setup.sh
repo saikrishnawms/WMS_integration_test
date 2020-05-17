b94bd4ef4df81c565fbea6b8ccc953d45a2def9f
Nishi Shriuvastava <nishishrivastava.91@gmail.com>
2020-05-17 02:34:17 +0200

    hellow

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ sudo apt-get -y update
 sudo apt-get install libxss1 libappindicator1 libindicator7 fonts-liberation libglib2.0-0=2.50.3-2 libnss3=2:3.26.2-1.1+deb9u1 libgconf-2-4=3.2.6-4+b1 libfontconfig1=2.11.0-6.7+b1
 sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome*.deb
+sudo apt-get -f install
 sudo wget https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip
 sudo unzip chromedriver_linux64.zip
 sudo mv chromedriver /usr/bin/chromedriver
